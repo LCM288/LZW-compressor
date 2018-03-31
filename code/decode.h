ed0444e847d600b30679fdc7989cfb75b67878ef
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 23:00:54 +0800

    Update decode.h

--- code/decode.h
+++ code/decode.h
@@ -1,6 +1,8 @@
-#include<dictionary.h>
-#include<ifstream>
-#include<ofstream>
+#include"dictionary.h"
+#include<fstream>
+
+#ifndef DECODE_H
+#define DECODE_H
 
 class decode {
 	private:
@@ -8,29 +10,30 @@ class decode {
 		std::ofstream output;				// output file stream
 		bits buffer;						// store the bits that have not been input
 		dictionary dict;					// the dictionary
-		void write(byte) const;				// write to output file
-		void write(bytes) const;			// write to output file
-		byte read() const;					// read a byte
+		void write(byte);					// write to output file
+		void write(bytes);					// write to output file
+		byte read();						// read a byte
 		bits read(int);						// read from input file
 	public:
 		void start_decode();				// start decode the file
 		decode(const char*, const char*);	// constructor
 		~decode();							// destructor
+};
+
+void decode::write(byte output_byte) {
+	char tmp = output_byte;
+	output.write(&tmp, 1);
 }
 
-void decode::write(byte output_byte) const {
-	output.write(&output_byte, 1);
-}
-
-void decode::write(bytes output_bytes) const {
+void decode::write(bytes output_bytes) {
 	for (auto output_byte: output_bytes)
 		write(output_byte);
 }
 
-byte decode::read() const {
-	byte input_byte;
-	input.read(&input_byte, 1);
-	return input_byte;
+byte decode::read() {
+	char tmp;
+	input.read(&tmp, 1);
+	return (unsigned char)(tmp);
 }
 
 bits decode::read(int no_of_bits) {
@@ -38,7 +41,7 @@ bits decode::read(int no_of_bits) {
 	if (int(buffer.size()) > no_of_bits) {									// if buffer is larger than required
 		buffer = bits(input_bits.begin() + no_of_bits, input_bits.end());
 		while (int(input_bits.size()) > no_of_bits)
-			no_of_bits.pop_back();
+			input_bits.pop_back();
 		return input_bits;
 	}
 	buffer.clear();
@@ -69,3 +72,5 @@ void decode::start_decode() {
 	while(!input.eof())
 		write(dict.decode(read(dict.next_length())));
 }
+
+#endif
