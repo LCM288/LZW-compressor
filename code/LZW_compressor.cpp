64e79126d064039f6abf7fe792609a75d8049a4a
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 23:33:55 +0800

    Update LZW_compressor.cpp
    this file is currently being used for checking and debugging

--- code/LZW_compressor.cpp
+++ code/LZW_compressor.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
+#include<cstring>
 #include"encode.h"
 #include"decode.h"
 
+char a[100], b[100], c[100];
+
 int main() {
+	scanf("%s", a);
+	if (!strcmp("encode", a)) {
+		scanf("%s %s", b, c);
+		encode x(b, c);
+		x.start_encode();
+	}
+	else if (!strcmp("decode", a)) {
+		scanf("%s %s", b, c);
+		decode x(b, c);
+		x.start_decode();
+	}
 }
