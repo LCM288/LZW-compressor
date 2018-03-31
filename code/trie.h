fc55273a112c1d0270ea13827d11976dbcb0739b
2013612 <samuelwong2013612@gmail.com>
2018-03-31 22:29:46 +0800

    Update trie.h

--- code/trie.h
+++ code/trie.h
@@ -20,8 +20,63 @@ class trie {
 		void reset();				// reset current position to root
 		trie();					// constructor
 		~trie();				// destructor
-		trie(const trie&);			// copy constructor
-		trie& operator= (const trie&);		// copy assignment
-		trie(trie&&);				// move constructor
-		trie& operator= (trie&&);		// move assignment
+}
+
+node::node(){
+	val = code(0);
+	for (int i = 0;i<256;i++){
+		child[i] = NULL;
+	}
+}
+
+node::node(code set_val){
+	val = set_val;
+	for (int i = 0;i<256;i++){
+		child[i] = NULL;
+	}
+}
+
+node::~node(){
+	for (int i=0;i<256;i++){
+		if (child[i] != NULL){
+			delete child[i];
+		}
+	}
+}
+
+static int trie::byte2int (byte character) const{
+	return int(character);
+}
+int trie::get_val() const{
+	return cur->val;
+}
+
+bool trie::exist_child(byte character) const{
+	if (child[byte2int(character)]!=NULL){
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void trie::add_new_node(byte character, code val){
+	cur -> child[byte2int(character)] = new node(val);
+}
+
+void trie::go_to_child(byte character){
+	cur = cur -> child[byte2int(character)];
+}
+void trie::reset(){
+	cur = root;
+}
+
+
+trie::trie(){
+	root = new node();
+	cur = root;
+}
+
+trie::~trie(){
+	delete root;
 }
