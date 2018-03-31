f9c5adf3d82374d45280c090f7f423e6d09ab160
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 11:58:49 +0800

    Create dictionary.h

---
+++ code/dictionary.h
@@ -0,0 +1,22 @@
+#include<vector>
+#include<bits.h>
+#include<trie.h>
+
+class dictionary {
+	private:
+		int _size;									// size of the dictionary
+		std::vector<bytes> words;					// corresponding bytes of the index
+		trie dictionary_tree;						// a trie storing the dictionary
+		bool check_exist(byte) const;				// check if adding next_byte still exists in dictionary_tree
+		void update_cur(byte);						// update current position of dictionary_tree
+	public:
+		int size() const;							// return the size of the dictionary
+		bits encode(byte)							// return the next encoded bits
+		bytes decode(bits)							// return the next decoded bytes
+		dictionary();								// constructor
+		~dictionary();								// desctructor
+		dictionary(const dictionary&);				// copy constructor
+		dictionary& operator= (const dictionary&);	// copy assignment
+		dictionary(dictionary&&);					// move constrctor
+		dictionary& operator= (dictionary&&);		// move assignment
+}
