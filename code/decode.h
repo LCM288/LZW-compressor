f92fc1c0589d99f1ed5b10a7fbf297829257993d
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 13:05:43 +0800

    Update decode.h

--- code/decode.h
+++ code/decode.h
@@ -4,13 +4,13 @@
 
 class decode {
 	private:
-		std::ifstream input;						// input file stream
-		std::ofstream output;						// output file stream
-		dictionary dict;						// the dictionary
-		void write(byte) const;						// write to output file
-		bits read() const;						// read from input file
+		std::ifstream input;				// input file stream
+		std::ofstream output;				// output file stream
+		dictionary dict;					// the dictionary
+		void write(byte) const;				// write to output file
+		bits read(int) const;				// read from input file
 	public:
-		start_decode() const;						// start decode the file
-		decode(const char*, const char*);				// constructor
+		start_decode() const;				// start decode the file
+		decode(const char*, const char*);	// constructor
 		~decode();							// destructor
 }
