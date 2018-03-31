2116b54826f852e3040feeb7802f4598a8c264c9
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 12:24:21 +0800

    Create decode.h

---
+++ code/decode.h
@@ -0,0 +1,16 @@
+#include<dictionary.h>
+#include<ifstream>
+#include<ofstream>
+
+class decode {
+	private:
+		std::ifstream input;						// input file stream
+		std::ofstream output;						// output file stream
+		dictionary dict;							// the dictionary
+		void write(byte) const;						// write to output file
+		bits read() const;							// read from input file
+	public:
+		start_decode() const;						// start decode the file
+		decode(const char*, const char*);			// constructor
+		~decode();									// destructor
+}
