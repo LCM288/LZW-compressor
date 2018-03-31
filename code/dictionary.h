8e26fa78982e426928a8405b04674cc69976e27f
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 14:53:25 +0800

    Update dictionary.h

--- code/dictionary.h
+++ code/dictionary.h
@@ -3,22 +3,24 @@
 
 class dictionary {
 	private:
-		int size, len;					// size of the dictionary, length of bits
-		std::vector<bytes> words;			// corresponding bytes of the index
-		trie dictionary_tree;				// a trie storing the dictionary
-		bool check_exist(byte) const;			// check if adding next_byte still exists in dictionary_tree
-		void update_cur(byte);				// update current position of dictionary_tree
-		void add_entry(byte);				// add new entry to dictionary_tree
-		static bits int2bits(int) const;		// convert integer to bits
-		void increase_size();				// increase size and len
+		int size, len;								// size of the dictionary, length of bits
+		bytes cur;									// cur bytes
+		std::vector<bytes> words;					// corresponding bytes of the index
+		trie dictionary_tree;						// a trie storing the dictionary
+		bool check_exist(byte) const;				// check if adding next_byte still exists in dictionary_tree
+		void update_cur(byte);						// update current position of dictionary_tree
+		void add_entry(byte);						// add new entry to dictionary_tree
+		static bits code2bits(code) const;			// convert code to bits
+		void increase_size();						// increase size and len
 	public:
-		bits encode(byte);				// return the next encoded bits
-		bytes decode(bits);				// return the next decoded bytes
-		dictionary();					// constructor
-		~dictionary();					// desctructor
-		dictionary(const dictionary&);			// copy constructor
+		int next_length() const;					// return the next encoded bits' length
+		bits encode(byte);							// return the next encoded bits
+		bytes decode(code);							// return the next decoded bytes
+		dictionary();								// constructor
+		~dictionary();								// desctructor
+		dictionary(const dictionary&);				// copy constructor
 		dictionary& operator= (const dictionary&);	// copy assignment
-		dictionary(dictionary&&);			// move constrctor
+		dictionary(dictionary&&);					// move constrctor
 		dictionary& operator= (dictionary&&);		// move assignment
 }
 
@@ -28,14 +30,17 @@ bool dictionary::check_exist(byte next_byte) const {
 
 void dictionary::update_cur(byte next_byte) {
 	dictionary_tree.go_to_child(next_byte);
+	cur.push_back(next_byte);
 }
 
-void dictionary:: add_entry(byte next_byte, int val) {
-	dictionary_tree.add_new_node(next_byte, val);
+void dictionary:: add_entry(byte next_byte) {
+	dictionary_tree.add_new_node(next_byte, code(size));
 	increase_size();
+	words.push_back(cur);
+	cur.clear();
 }
 
-static bits dictionary::int2bits(int val) const {
+static bits dictionary::code2bits(code val) const {
 	bits tmp;
 	for (int i = 0; i < len; i++) {
 		tmp.push_back(std::bitset<1> (val % 2));
@@ -51,15 +56,31 @@ void dictionary::increase_size() {
 	size++;
 }
 
+int dictionary::next_length() const {
+	if (size == (size & -size))
+		return len + 1;
+	else
+		return len;
+}
+
 bits dictionary::encode(byte next_byte) {
 	if (check_exist(next_byte)) {
 		update_cur(next_byte);
 		return bits(0);
 	}
 	else {
-		add_entry(next_byte, size);
-		int val = dictionary_tree.get_val();
+		add_entry(next_byte);
+		code val = dictionary_tree.get_val();
 		dictionary_tree.reset();
-		return int2bits(val);
+		return code2bits(val);
 	}
+}
+
+bytes dictionary::decode(code val) {
+	bytes data = words[int(val)];
+	if (cur.size())
+		add_entry(data[0]);
+	for (auto next_byte: data)
+		update_cur(next_byte);
+	return data;
 }
