e9a9a483cb3c232996a8597ecfc40061f5ead9fc
LCM288 <charlieli@hotmail.com.tw>
2018-03-31 12:24:47 +0800

    Update encode.h

--- code/encode.h
+++ code/encode.h
@@ -6,11 +6,11 @@ class encode {
 	private:
 		std::ifstream input;						// input file stream
 		std::ofstream output;						// output file stream
-		dictionary dict;							// the dictionary
+		dictionary dict;						// the dictionary
 		void write(bits) const;						// write to output file
-		byte read() const;							// read from input file
+		byte read() const;						// read from input file
 	public:
 		start_encode() const;						// start encode the file
-		encode(const char*, const char*);			// constructor
-		~encode();									// destructor
+		encode(const char*, const char*);				// constructor
+		~encode();							// destructor
 }
