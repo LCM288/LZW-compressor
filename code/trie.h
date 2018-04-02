891c7bb8a87d08088258c66580c43324c9c03b16
LCM288 <charlieli@hotmail.com.tw>
2018-04-03 02:34:55 +0800

    Update trie.h

--- code/trie.h
+++ code/trie.h
@@ -1,11 +1,16 @@
 #include"bits.h"
+#include<map> 
 
 #ifndef TRIE_H
 #define TRIE_H
 
 struct node {
 		code val;							// value stored on the node
+		#ifdef USE_MAP
+		std::map<byte, node*> child;
+		#else
 		node* child[256];					// children of the node
+		#endif
 		node();								// constructor
 		node(code);							// constructor
 		~node();							// destructor
@@ -14,66 +19,83 @@ struct node {
 class trie {
 	private:
 		node *cur, *root;					// store the current position and the root of the trie
-		static int byte2int (byte);			// convert byte to int (0~255)
 	public:
 		code get_val() const;				// get value of current position
 		bool exist_child(byte) const;		// check if current node has a corresponding child
 		void add_new_node(byte, code);		// add a new child for current node
 		void go_to_child(byte);				// change current position to the child node
 		void reset();						// reset current position to root
+		void reconstruct();					// reconstruct the trie
 		trie();								// constructor
 		~trie();							// destructor
 };
 
 node::node(){
 	val = code(0);
-	for (int i = 0;i<256;i++){
+	#ifndef USE_MAP
+	for (int i = 0; i < 256; i++)
 		child[i] = NULL;
-	}
+	#endif
 }
 
 node::node(code set_val){
 	val = set_val;
-	for (int i = 0;i<256;i++){
+	#ifndef USE_MAP
+	for (int i = 0; i < 256; i++)
 		child[i] = NULL;
-	}
+	#endif
 }
 
 node::~node(){
-	for (int i=0;i<256;i++){
-		if (child[i] != NULL){
+	for (int i = 0; i < 256; i++){
+		#ifdef USE_MAP
+		if (child.find(byte(i)) != child.end())
+			delete child[byte(i)];
+		#else
+		if (child[i] != NULL)
 			delete child[i];
-		}
+		#endif
 	}
 }
 
-int trie::byte2int (byte character) {
-	return int(character);
-}
 code trie::get_val() const{
-	return cur->val;
+	return cur -> val;
 }
 
 bool trie::exist_child(byte character) const{
-	if (cur -> child[byte2int(character)]!=NULL){
+	#ifdef USE_MAP
+	if (cur -> child.find(character) != cur -> child.end())
+	#else
+	if (cur -> child[int(character)] != NULL)
+	#endif
 		return true;
-	}
-	else {
+	else
 		return false;
-	}
 }
 
 void trie::add_new_node(byte character, code val){
-	cur -> child[byte2int(character)] = new node(val);
+	#ifdef USE_MAP
+	cur -> child[character] = new node(val);
+	#else
+	cur -> child[int(character)] = new node(val);
+	#endif
 }
 
 void trie::go_to_child(byte character){
-	cur = cur -> child[byte2int(character)];
+	#ifdef USE_MAP
+	cur = cur -> child[character];
+	#else
+	cur = cur -> child[int(character)];
+	#endif
 }
 void trie::reset(){
 	cur = root;
 }
 
+void trie::reconstruct() {
+	delete root;
+	cur = root = new node();
+}
 
 trie::trie(){
 	root = new node();
@@ -83,5 +105,4 @@ trie::trie(){
 trie::~trie(){
 	delete root;
 }
-
 #endif
