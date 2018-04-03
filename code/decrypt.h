5ca3eb66c7c4d4847b6e06e2c2f1e84f7b0de957
LCM288 <charlieli@hotmail.com.tw>
2018-04-03 18:41:26 +0800

    Create decrypt.h

---
+++ code/decrypt.h
@@ -0,0 +1,16 @@
+#include<fstream>
+
+#ifndef DECRYPT_H
+#define DECRYPT_H
+
+class decrypt: public AES {
+	private:
+		ifstream input;					// input file stream
+		void read_n_decrypt();				// read a block from file and decrypt
+	public:
+		bytes get_next_decrypted();				// get the next decrypted byte
+		bool finished();					// return true if block is empty
+		decrypt(const char* password, const char* input_file)	// constructor
+};
+		
+#endif
