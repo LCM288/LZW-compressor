3a3bd2ab5e683cf11ed20d6b493ecced8f768f36
LCM288 <charlieli@hotmail.com.tw>
2018-04-04 00:01:18 +0800

    Update decrypt.h

--- code/decrypt.h
+++ code/decrypt.h
@@ -6,13 +6,39 @@
 
 class decrypt: public AES {
 	private:
-		ifstream input;					// input file stream
-		void read_n_decrypt();				// read a block from file and decrypt
+		std::ifstream input;						// input file stream
+		void read_n_decrypt();						// read a block from file and decrypt
 	public:
-		bytes get_next_decrypted();				// get the next decrypted byte
-		bool finished();					// return true if block is empty
-		decrypt();						// constructor
-		decrypt(const char* password, const char* input_file);	// constructor
+		byte get_next_decrypted();					// get the next decrypted byte
+		bool finished();							// return true if block is empty
+		void set_decrypt(const char*, const char*);	// set up the variables
+		decrypt() {}								// constructor
 };
-		
+
+void decrypt::read_n_decrypt() {
+	char tmp[16];
+	input.read(tmp, 16);
+	for (int i = 0; i < 16; i++)
+		block.push_back(tmp[i]);
+	// do the decryption here
+}
+
+byte decrypt::get_next_decrypted() {
+	if (block.size() == 0)
+		read_n_decrypt();
+	byte next_byte = block[0];
+	block.erase(block.begin());
+	return next_byte;
+}
+/*
+bool decrypt::finished() {
+	return input.eof() && block.empty();
+}
+*/
+void decrypt::set_decrypt(const char* password, const char* input_file) {
+	input.open(input_file, std::ios::in | std::ios::binary);
+	input.seekg(8, std::ios::beg);
+}
+
+
 #endif
