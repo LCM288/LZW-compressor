070b88321fccba4e9bb7f8bca631f5c5f0d4365a
LCM288 <charlieli@hotmail.com.tw>
2018-04-03 23:59:11 +0800

    Update encrypt.h

--- code/encrypt.h
+++ code/encrypt.h
@@ -6,13 +6,37 @@
 
 class encrypt: public AES {
 	private:
-		ofstream output;				// output file stream
-		void encrypt_n_write();				// encrypt the block and write to file
+		std::ofstream output;						// output file stream
+		void encrypt_n_write();						// encrypt the block and write to file
 	public:
-		void add_to_encrypt(byte byte_to_encrypt);		// add the next byte to encrypt
-		bool finished();					// return true if block is empty
-		encrypt();						// constructor
-		encrypt(const char* password, const char* output_file);	// constructor
+		void add_to_encrypt(byte);					// add the next byte to encrypt
+		bool finished();							// return true if block is empty
+		void set_encrypt(const char*, const char*);	// set up the variables
+		encrypt() {}								// constructor
 };
+
+void encrypt::encrypt_n_write() {
+	// do the encryption here
+	for (auto next_byte: block) {
+		char tmp = char(next_byte);
+		output.write(&tmp, 1);
+	}
+	block.clear();
+}
+
+void encrypt::add_to_encrypt(byte byte_to_encrypt) {
+	block.push_back(byte_to_encrypt);
+	if (block.size() == 16)
+		encrypt_n_write();
+}
+
+bool encrypt::finished() {
+	return block.empty();
+}
+
+void encrypt::set_encrypt(const char* password, const char* output_file) {
+	output.open(output_file, std::ios::out | std::ios::binary);
+	output.seekp(8, std::ios::beg);
+}
 
 #endif
