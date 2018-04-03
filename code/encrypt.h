5abfdac1d8797027eaedf4ae04277ae3e7e8af62
LCM288 <charlieli@hotmail.com.tw>
2018-04-03 18:40:05 +0800

    Create encrypt.h

---
+++ code/encrypt.h
@@ -0,0 +1,16 @@
+#include<fstream>
+
+#ifndef ENCRYPT_H
+#define ENCRYPT_H
+
+class encrypt: public AES{
+	private:
+		ofstream output;				// output file stream
+		void encrypt_n_write();				// encrypt the block and write to file
+	public:
+		void add_to_encrypt(byte byte_to_encrypt);		// add the next byte to encrypt
+		bool finished();					// return true if block is empty
+		encrypt(const char* password, const char* output_file)	// constructor
+};
+
+#endif
